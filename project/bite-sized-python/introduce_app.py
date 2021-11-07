56ef2856868a375bc729b7fc00c055587fb73ae8
Marjy Guery <mguery@users.noreply.github.com>
2021-11-06 21:29:12 -0400

    Create introduce_app.py

---
+++ project/bite-sized-python/introduce_app.py
@@ -0,0 +1,9 @@
+print('Welcome back to school! Answer these 3 questions to introduce yourself!')
+
+name = input('What is your name? ') # test code print(name)
+color = input('Fave color? ')
+food = input('Fave food? ')
+show = input('Fave show? ')
+
+# print vars - print(f'sentence and {var}.')
+print(f'Ok folks, meet {name}. Their fave color is {color}, fave food is {food}, and fave show is {show}.')
