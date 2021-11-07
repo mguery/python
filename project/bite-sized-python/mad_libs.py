fd9dad3fbeebc3fdb963e42e1ee99f90aa7868b7
Marjy Guery <mguery@users.noreply.github.com>
2021-11-06 22:58:47 -0400

    Create mad_libs.py

---
+++ project/bite-sized-python/mad_libs.py
@@ -0,0 +1,11 @@
+# Prompts
+adjective1 = input('Enter adjective: ').lower()
+game = input('Enter name of an outdoor game: ').lower()
+adjective2 = input('Enter 2nd adjective: ').lower()
+friend = input('Enter name of a friend: ').capitalize()
+verb = input('Enter verb ending in ing: ').lower()
+adjective3 = input('Enter 3rd adjective: ').lower()
+
+story = (f'some random adjectives for this mad libs are {adjective1}, {adjective2}, and {adjective3}. The last game I watched was {game} with my friend {friend}. We both thought it was {verb}.')
+
+print(story)
